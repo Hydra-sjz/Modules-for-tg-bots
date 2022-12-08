dcdb882506824d2d420bfd60b339d4c393f5f3bf
Hydra-sjz <119828208+Hydra-sjz@users.noreply.github.com>
2022-12-08 20:19:36 +0530

    Add files via upload

---
+++ database/filters.py
@@ -0,0 +1,10 @@
+from pyrogram import filters
+from typing import List, Union
+from config import COMMAND_PREFIXES
+
+
+other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+
+
+def command(commands: Union[str, List[str]]):
+    return filters.command(commands, COMMAND_PREFIXES)
