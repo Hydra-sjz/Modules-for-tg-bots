83e8c2ce32ebcde7edc71c36d9f9a61889532205
Hydra-sjz <119828208+Hydra-sjz@users.noreply.github.com>
2023-02-08 22:20:47 +0530

    Update __init__.py

--- mbot/__init__.py
+++ mbot/__init__.py
@@ -28,10 +28,22 @@ from dotenv import load_dotenv
 from pyrogram import Client
 
 
+
 from aiohttp import web
 from mbot import web_server
-
 from config import PORT
+
+
+from mbot.route import routes
+
+
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
+
+
+
 
 load_dotenv("config.env")
 
