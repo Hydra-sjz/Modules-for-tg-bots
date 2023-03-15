efa5b98d3031537024c1b28f64321a998a8398d6
Hydra ☠️ <119828208+Hydra-sjz@users.noreply.github.com>
2023-03-15 12:09:41 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Hydra-sjz/Spotify-dl-bot-tg.git /Spotify-dl-bot-tg
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Spotify-dl-bot-tg
+fi
+cd /Spotify-dl-bot-tg
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
