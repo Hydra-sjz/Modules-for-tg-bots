96e685b3b43c5684da4c59bdf04e98437cff85a5
Hydra ☠️ <119828208+Hydra-sjz@users.noreply.github.com>
2023-04-10 13:13:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,6 +14,11 @@ SUDO_USERS = list(map(int, getenv("SUDO_USERS", "784589736").split()))
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001784386455"))
 
 
+#LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001784386455"))
+AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "784589736").split())
+DB_URL = os.environ.get("DB_URL", "")
+DB_NAME = os.environ.get("DB_NAME", "")
+BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
 
 
 
