a070a0013d8215b7bf2296eabc392205f9ca8c44
Hydra ☠️ <119828208+Hydra-sjz@users.noreply.github.com>
2023-04-11 20:43:19 +0530

    Update start.py

--- mbot/plugins/start.py
+++ mbot/plugins/start.py
@@ -273,3 +273,120 @@ ABOUT_BUTTONS = InlineKeyboardMarkup(
         ]]
     )
 
+#==================•BROADCAST•==================
+@Client.on_message(filters.private & filters.command("broadcast"))
+async def broadcast_handler_open(_, m):
+    if m.from_user.id not in AUTH_USERS:
+        await m.delete()
+        return
+    if m.reply_to_message is None:
+        await m.delete()
+    else:
+        await broadcast(m, db)
+
+@Client.on_message(filters.private & filters.command("stats"))
+async def sts(c, m):
+    if m.from_user.id not in AUTH_USERS:
+        await m.delete()
+        return
+    await m.reply_text(
+        text=f"**Total Users in Database 📂:** `{await db.total_users_count()}`\n\n**Total Users with Notification Enabled 🔔 :** `{await db.total_notif_users_count()}`",
+        quote=True
+    )
+
+@Client.on_message(filters.private & filters.command("ban_user"))
+async def ban(c, m):
+    if m.from_user.id not in AUTH_USERS:
+        await m.delete()
+        return
+    if len(m.command) == 1:
+        await m.reply_text(
+            f"Use this command to ban 🛑 any user from the bot 🤖.\n\nUsage:\n\n`/ban_user user_id ban_duration ban_reason`\n\nEg: `/ban_user 1234567 28 You misused me.`\n This will ban user with id `1234567` for `28` days for the reason `You misused me`.",
+            quote=True,
+        )
+        return
+
+    try:
+        user_id = int(m.command[1])
+        ban_duration = int(m.command[2])
+        ban_reason = " ".join(m.command[3:])
+        ban_log_text = f"Banning user {user_id} for {ban_duration} days for the reason {ban_reason}."
+
+        try:
+            await c.send_message(
+                user_id,
+                f"You are Banned 🚫 to use this bot for **{ban_duration}** day(s) for the reason __{ban_reason}__ \n\n**Message from the admin 🤠**",
+            )
+            ban_log_text += "\n\nUser notified successfully!"
+        except BaseException:
+            traceback.print_exc()
+            ban_log_text += (
+                f"\n\n ⚠️ User notification failed! ⚠️ \n\n`{traceback.format_exc()}`"
+            )
+        await db.ban_user(user_id, ban_duration, ban_reason)
+        print(ban_log_text)
+        await m.reply_text(ban_log_text, quote=True)
+    except BaseException:
+        traceback.print_exc()
+        await m.reply_text(
+            f"Error occoured ⚠️! Traceback given below\n\n`{traceback.format_exc()}`",
+            quote=True
+        )
+
+@Client.on_message(filters.private & filters.command("unban_user"))
+async def unban(c, m):
+    if m.from_user.id not in AUTH_USERS:
+        await m.delete()
+        return
+    if len(m.command) == 1:
+        await m.reply_text(
+            f"Use this command to unban 😃 any user.\n\nUsage:\n\n`/unban_user user_id`\n\nEg: `/unban_user 1234567`\n This will unban user with id `1234567`.",
+            quote=True,
+        )
+        return
+
+    try:
+        user_id = int(m.command[1])
+        unban_log_text = f"Unbanning user 🤪 {user_id}"
+
+        try:
+            await c.send_message(user_id, f"Your ban was lifted!")
+            unban_log_text += "\n\n✅ User notified successfully! ✅"
+        except BaseException:
+            traceback.print_exc()
+            unban_log_text += (
+                f"\n\n⚠️ User notification failed! ⚠️\n\n`{traceback.format_exc()}`"
+            )
+        await db.remove_ban(user_id)
+        print(unban_log_text)
+        await m.reply_text(unban_log_text, quote=True)
+    except BaseException:
+        traceback.print_exc()
+        await m.reply_text(
+            f"⚠️ Error occoured ⚠️! Traceback given below\n\n`{traceback.format_exc()}`",
+            quote=True,
+        )
+
+@Client.on_message(filters.private & filters.command("banned_users"))
+async def _banned_usrs(c, m):
+    if m.from_user.id not in AUTH_USERS:
+        await m.delete()
+        return
+    all_banned_users = await db.get_all_banned_users()
+    banned_usr_count = 0
+    text = ""
+    async for banned_user in all_banned_users:
+        user_id = banned_user["id"]
+        ban_duration = banned_user["ban_status"]["ban_duration"]
+        banned_on = banned_user["ban_status"]["banned_on"]
+        ban_reason = banned_user["ban_status"]["ban_reason"]
+        banned_usr_count += 1
+        text += f"> **User_id**: `{user_id}`, **Ban Duration**: `{ban_duration}`, **Banned on**: `{banned_on}`, **Reason**: `{ban_reason}`\n\n"
+    reply_text = f"Total banned user(s) 🤭: `{banned_usr_count}`\n\n{text}"
+    if len(reply_text) > 4096:
+        with open("banned-users.txt", "w") as f:
+            f.write(reply_text)
+        await m.reply_document("banned-users.txt", True)
+        os.remove("banned-users.txt")
+        return
+    await m.reply_text(reply_text, True)
