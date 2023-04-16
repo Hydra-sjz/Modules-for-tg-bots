4a2923b52d5ae58c550272f8770339a53fbd7a0c
Hydra ☠️ <119828208+Hydra-sjz@users.noreply.github.com>
2023-04-16 11:46:50 +0530

    Update __main__.py

--- mbot/__main__.py
+++ mbot/__main__.py
@@ -1,8 +1,13 @@
 from os import mkdir, path
 
-from mbot import Mbot
+from mbot import Mbot, bot, logger
 
 if __name__ == "__main__":
     if not path.exists("cache"):
         mkdir("cache")
     Mbot().run()
+
+with bot:
+    bot.run_until_disconnected()
+    logger.info('BOT STOPPED BROOO')
+    bot.loop.run_until_complete(fetch.session.close())
