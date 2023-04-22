cf24db3cc88c45a5762c12718b60f4e0bee73656
Hydra ☠️ <119828208+Hydra-sjz@users.noreply.github.com>
2023-04-23 02:55:37 +0530

    Update mentionall.py

--- mbot/plugins/mentionall.py
+++ mbot/plugins/mentionall.py
@@ -5,12 +5,12 @@ from telethon.errors import UserNotParticipantError
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 
-from mbot import bot as tbot
+from mbot import bot
 
 spam_chats = []
 
 
-@tbot.on(
+@bot.on(
     events.NewMessage(
         pattern="^/tagall|/call|/tall|/all|/mentionall|#all|@all|@mentionall|@tagall|@utag(.*)",
     )
@@ -22,7 +22,7 @@ async def all(event):
 
     is_admin = False
     try:
-        partici_ = await tbot(GetParticipantRequest(event.chat_id, event.sender_id))
+        partici_ = await bot(GetParticipantRequest(event.chat_id, event.sender_id))
     except UserNotParticipantError:
         is_admin = False
     else:
@@ -52,7 +52,7 @@ async def all(event):
     spam_chats.append(chat_id)
     usrnum = 0
     usrtxt = ""
-    async for usr in tbot.iter_participants(chat_id):
+    async for usr in bot.iter_participants(chat_id):
         if chat_id not in spam_chats:
             break
         usrnum += 1
@@ -60,7 +60,7 @@ async def all(event):
         if usrnum == 10:
             if mode == "text_on_cmd":
                 txt = f"{usrtxt}\n\n{msg}\n\nMeet Me Here🙈 @Besties_XD ✨🥀"
-                await tbot.send_message(chat_id, txt)
+                await bot.send_message(chat_id, txt)
             elif mode == "text_on_reply":
                 await msg.reply(usrtxt)
             await asyncio.sleep(2)
@@ -72,7 +72,7 @@ async def all(event):
         pass
 
 
-@tbot.on(events.NewMessage(pattern="^/cancel$"))
+@bot.on(events.NewMessage(pattern="^/cancel$"))
 async def cancel_spam(event):
     if event.chat_id not in spam_chats:
         return await event.respond("There Is No Proccess On Going")
