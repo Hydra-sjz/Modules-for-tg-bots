d4d5e83bd54b59236ab5cf618d70fd01422fac1a
Hydra ☠️ <119828208+Hydra-sjz@users.noreply.github.com>
2023-04-22 18:48:48 +0530

    Update mentionall.py

--- mbot/plugins/mentionall.py
+++ mbot/plugins/mentionall.py
@@ -29,7 +29,7 @@ async def all(event):
         if isinstance(
             partici_.participant, (ChannelParticipantAdmin, ChannelParticipantCreator)
         ):
-            is_admin = True
+            is_admin = False
     if not is_admin:
         return await event.respond("Only admins can mention all!")
 
