46acdc8a0f9f787fd8929685f418f9b264fddca8
Hydra ☠️ <119828208+Hydra-sjz@users.noreply.github.com>
2023-04-22 20:02:40 +0530

    Create encript.py

---
+++ mbot/plugins/encript.py
@@ -0,0 +1,36 @@
+from mbot import bot as tbot
+from mbot.events2 import register
+import secureme
+
+
+@register(pattern="^/encrypt ?(.*)")
+async def hmm(event):
+    if event.reply_to_msg_id:
+        lel = await event.get_reply_message()
+        cmd = lel.text
+    else:
+        cmd = event.pattern_match.group(1)
+    Text = cmd
+    k = secureme.encrypt(Text)
+    await event.reply(k)
+
+
+@register(pattern="^/decrypt ?(.*)")
+async def hmm(event):
+    if event.reply_to_msg_id:
+        lel = await event.get_reply_message()
+        ok = lel.text
+    else:
+        ok = event.pattern_match.group(1)
+    Text = ok
+    k = secureme.decrypt(Text)
+    await event.reply(k)
+
+
+__mod_name__ = "Encryption"
+
+___help__ = """
+Here is help for Encryption
+Note: Only works for text.
+ ❍ `/encrypt` - Reply to a text message to Encrypt it.
+ ❍ `/decrypt` - Reply to a text message to Decrypt it
