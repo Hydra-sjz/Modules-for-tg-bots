fc454a16df488ae1e718cc7faccdb186dbfcf7f8
Hydra ☠️ <119828208+Hydra-sjz@users.noreply.github.com>
2023-04-23 20:43:42 +0530

    Update MongoDb.py

--- mbot/database/MongoDb.py
+++ mbot/database/MongoDb.py
@@ -2,8 +2,8 @@ from sys import exit as exiter
 
 from motor.motor_asyncio import AsyncIOMotorClient
 
-from mbot.config import DB_URI
-from TelegramBot.logging import LOGGER
+from config import DB_URI
+from mbot.logging import LOGGER
 
 
 # from one string uri you can create multiple databases for different projects/bots. within each database you can store multiple collections, and within each collection you can store multiple documents.
