53d7cd674d36c3084a96db66b06f8ff99f83b912
Hydra ☠️ <119828208+Hydra-sjz@users.noreply.github.com>
2023-04-23 21:12:27 +0530

    Update filters.py

--- mbot/helpers/filters.py
+++ mbot/helpers/filters.py
@@ -1,14 +1,14 @@
 from pyrogram import filters
 from pyrogram.types import Message
-from config import SUDO_USERID, OWNER_USERID
+from mbot import SUDO_USERS, OWNER_ID
 
 
 def dev_users(_, __, message: Message) -> bool:
-    return message.from_user.id in OWNER_USERID if message.from_user else False
+    return message.from_user.id in OWNER_ID if message.from_user else False
 
 
 def sudo_users(_, __, message: Message) -> bool:
-    return message.from_user.id in SUDO_USERID if message.from_user else False
+    return message.from_user.id in SUDO_USERS if message.from_user else False
 
 
 dev_cmd = filters.create(dev_users)
