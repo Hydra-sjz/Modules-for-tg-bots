b6ae57cbe2d1346092bc9503150c319151711bd0
Hydra ☠️ <119828208+Hydra-sjz@users.noreply.github.com>
2023-04-24 15:30:12 +0530

    Update snap.py

--- mbot/plugins/snap.py
+++ mbot/plugins/snap.py
@@ -4,7 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 from pyrogram import Client, filters
 
-from nbot import LOGGER
+from mbot import LOGGER
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"
