8d055b98b68ec762f062638b5b66f37f39bd14d4
Hydra ☠️ <119828208+Hydra-sjz@users.noreply.github.com>
2023-04-25 22:56:23 +0530

    Update db.py

--- mbot/ani_utils/db.py
+++ mbot/ani_utils/db.py
@@ -11,7 +11,7 @@ print("Connecting to Database ...")
 _MGCLIENT: AgnosticClient = AsyncIOMotorClient(DB_URL)
 _RUN = asyncio.get_event_loop().run_until_complete
 
-if "anibot" in _RUN(_MGCLIENT.list_database_names()):
+if "mbot" in _RUN(_MGCLIENT.list_database_names()):
     print("anibot Database Found :) => Now Logging to it...")
 else:
     print("anibot Database Not Found :( => Creating New Database...")
