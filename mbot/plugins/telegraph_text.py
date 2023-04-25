002109117aba792bc8087698ff019a1d6fc9a6e9
Hydra ☠️ <119828208+Hydra-sjz@users.noreply.github.com>
2023-04-25 19:15:04 +0530

    Create telegraph_text.py

---
+++ mbot/plugins/telegraph_text.py
@@ -0,0 +1,24 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from mbot import Mbot as app, telegraph
+
+
+@app.on_message(filters.command("telegraph"))
+async def paste(_, message: Message):
+    reply = message.reply_to_message
+
+    if not reply or not reply.text:
+        return await message.reply("Reply to a text message")
+
+    if len(message.command) < 2:
+        return await message.reply("**Usage:**\n /telegraph [Page name]")
+
+    page_name = message.text.split(None, 1)[1]
+    page = telegraph.create_page(
+        page_name, html_content=reply.text.html.replace("\n", "<br>")
+    )
+    return await message.reply(
+        f"**Posted:** {page['url']}",
+        disable_web_page_preview=True,
+    )
