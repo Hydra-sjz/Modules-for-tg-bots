39566bd98192b5ebd309540affa1ffc23adbd1a4
Hydra ☠️ <119828208+Hydra-sjz@users.noreply.github.com>
2023-04-25 21:03:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,3 +32,14 @@ AUTO_DELETE = bool(environ.get("AUTO_DELETE", True))
 AUTO_DELETE_SECOND = int(environ.get("AUTO_DELETE_SECOND", 300))
 
 OPENWEATHERMAP_ID = os.environ.get("OPENWEATHERMAP_ID", None)
+
+
+BOT_NAME = os.environ.get("BOT_NAME")
+ANILIST_CLIENT = os.environ.get("ANILIST_CLIENT")
+ANILIST_SECRET = os.environ.get("ANILIST_SECRET")
+ANILIST_REDIRECT_URL = os.environ.get("ANILIST_REDIRECT_URL", "https://anilist.co/api/v2/oauth/pin")
+
+TRIGGERS = os.environ.get("TRIGGERS", "/ !").split()
+
+DOWN_PATH = "anibot/downloads/"
+HELP_DICT = dict()
