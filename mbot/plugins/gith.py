ba6fdcc937f97d9a88abad93133381b95343bed3
Hydra ☠️ <119828208+Hydra-sjz@users.noreply.github.com>
2023-04-26 11:11:50 +0530

    Create gith.py

---
+++ mbot/plugins/gith.py
@@ -0,0 +1,42 @@
+from urllib.parse import quote_plus
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from mbot import Mbot
+from mbot.utils import commands, http
+
+
+@Mbot.on_message(filters.command(["git", "github"]))
+async def git(c: Client, m: Message, strings):
+    if len(m.command) == 1:
+        return await m.reply_text(
+            strings("no_username_err"), reply_to_message_id=m.id
+        )
+    text = m.text.split(maxsplit=1)[1]
+    req = await http.get(f"https://api.github.com/users/{text}")
+    res = req.json()
+
+    if not res.get("login"):
+        return await m.reply_text(
+            strings("not_found_user"), reply_to_message_id=m.id
+        )
+
+    avatar = res["avatar_url"]
+
+    anticache = quote_plus((await http.head(avatar)).headers["Last-Modified"])
+
+    caption_text = strings("info_git_user")
+    await m.reply_photo(
+        avatar + anticache,
+        caption=caption_text.format(
+            name=res["name"],
+            username=res["login"],
+            location=res["location"],
+            type=res["type"],
+            bio=res["bio"],
+        ),
+        reply_to_message_id=m.id,
+    )
+
+
+commands.add_command("git", "tools")
