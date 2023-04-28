7a52e3ec13ef2431ca53733ce3a095426d592e51
Hydra ☠️ <119828208+Hydra-sjz@users.noreply.github.com>
2023-04-28 19:51:52 +0530

    Update __init__.py

--- mbot/__init__.py
+++ mbot/__init__.py
@@ -1,39 +1,21 @@
-"""MIT License
 
-Copyright (c) 2022 Daniel
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
 
 import os
 import logging
 from os import environ, mkdir, path, sys
-
 from dotenv import load_dotenv
 from pyrogram import Client
 from telegraph import Telegraph
-
+from aiohttp import ClientSession
 import time
 import deethon
 from dotenv import load_dotenv
 from telethon import TelegramClient, events, functions, types
 from telegraph import Telegraph
+
+
+
+
 
 log_chat: int = -1001616139929
 telegraph = Telegraph()
@@ -61,11 +43,7 @@ telethon_logger.addHandler(fh)
 
 botStartTime = time.time()
 
-
-
 load_dotenv()
-
-
 
 # Saving user preferences locally
 users = {}
@@ -95,6 +73,18 @@ except KeyError:
 
 # Optional Variable
 
+DOWN_PATH = "mbot/downloads/"
+HELP_DICT = dict()
+
+LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "-1001616139929"))
+OWNER = list(filter(lambda x: x, map(int, os.environ.get("OWNER_ID", "784589736 5422115985").split())))  ## sudos can be included
+ANILIST_CLIENT = os.environ.get("ANILIST_CLIENT")
+ANILIST_SECRET = os.environ.get("ANILIST_SECRET")
+ANILIST_REDIRECT_URL = os.environ.get("ANILIST_REDIRECT_URL", "https://anilist.co/api/v2/oauth/pin")
+DB_URL = os.environ.get("DB_URL")
+BOT_NAME = os.environ.get("BOT_NAME", "spoti")
+TRIGGERS = os.environ.get("TRIGGERS", "/ !").split()
+
 SUDO_USERS = environ.get("SUDO_USERS", str(OWNER_ID)).split()
 SUDO_USERS = [int(_x) for _x in SUDO_USERS]
 if OWNER_ID not in SUDO_USERS:
@@ -115,6 +105,11 @@ logger.info("TELETHON STARTED BROOO")
 
 #========lol===========
 
+
+
+session = ClientSession()
+plugins = dict(root="mbot/plugins")
+
 class Mbot(Client):
     def __init__(self):
         name = self.__class__.__name__.lower()
@@ -126,6 +121,7 @@ class Mbot(Client):
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
             sleep_threshold=30,
+            plugins=plugins
         )
 
     async def start(self):
@@ -138,7 +134,7 @@ class Mbot(Client):
             await self.send_photo(
                 chat,
                 "https://telegra.ph/file/5791b80b0c4349c85c604.jpg",
-                "**Bot Started.** 🎵",
+                "😈😈😈**Bot Started.**😈😈😈",
             )
         LOGGER.info(f"PYROGRAM STARTED AS {BOT_INFO.username}\n")
 
