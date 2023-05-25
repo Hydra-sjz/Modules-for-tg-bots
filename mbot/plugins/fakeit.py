02879605d21d96a95b13e869d0166ae6fed72527
Hydra <119828208+Hydra-sjz@users.noreply.github.com>
2023-05-25 14:54:35 +0530

    Create fakeit.py
    Signed-off-by: Hydra  <119828208+Hydra-sjz@users.noreply.github.com>

---
+++ mbot/plugins/fakeit.py
@@ -0,0 +1,28 @@
+from faker import Faker
+from faker.providers import internet
+from pyrogram import filters
+from telethon import events
+from mbot import Mbot as pbot
+from mbot import bot as tbot
+@tbot.on(events.NewMessage(pattern="/fakegen$"))
+async def hi(event):
+    fake = Faker()
+    print("FAKE DETAILS GENERATED\n")
+    name = str(fake.name())
+    fake.add_provider(internet)
+    address = str(fake.address())
+    ip = fake.ipv4_private()
+    cc = fake.credit_card_full()
+    email = fake.ascii_free_email()
+    job = fake.job()
+    android = fake.android_platform_token()
+    pc = fake.chrome()
+    await event.reply(
+        f"<b><u> Fake Information Generated</b></u>\n<b>Name :-</b><code>{name}</code>\n\n<b>Address:-</b><code>{address}</code>\n\n<b>IP ADDRESS:-</b><code>{ip}</code>\n\n<b>credit card:-</b><code>{cc}</code>\n\n<b>Email Id:-</b><code>{email}</code>\n\n<b>Job:-</b><code>{job}</code>\n\n<b>android user agent:-</b><code>{android}</code>\n\n<b>Pc user agent:-</b><code>{pc}</code>",
+        parse_mode="HTML",
+    )
+@pbot.on_message(filters.command("picgen"))
+async def picgen(_, message):
+    img = "https://thispersondoesnotexist.com/image"
+    text = f"Fake Image successfully generated."
+    await message.reply_photo(photo=img, caption=text)
